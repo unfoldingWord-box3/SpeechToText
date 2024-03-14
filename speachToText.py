e370e28b9c0d948ad5d8cf14186d468ae6b3063d
PhotoNomad0 <bruce.mclean@unfoldingword.org>
2024-03-14 09:33:59 -0400

    added French and YLB

--- speachToText.py
+++ speachToText.py
@@ -1,6 +1,10 @@
 from faster_whisper import WhisperModel
 
 model_size = "large-v3"
+# sourceAudioFile = "./speech/obs/en/en_obs_02-05_128kbps.mp3"
+# sourceAudioFile = "speech/obs/fr/fr_obs_01_128kbps.mp3"
+sourceAudioFile = "speech/obs/ylb/ylb_obs_09_128kbps.mp3"
+
 
 # Run on GPU with FP16
 # model = WhisperModel(model_size, device="cuda", compute_type="float16")
@@ -10,9 +14,10 @@ model_size = "large-v3"
 # or run on CPU with INT8
 # model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
+print("Opening Model")
 model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
-sourceAudioFile = "./speech/obs/en/en_obs_02-05_128kbps.mp3"
+print("Transcribing Audio")
 segments, info = model.transcribe(sourceAudioFile, beam_size=5)
 
 print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
